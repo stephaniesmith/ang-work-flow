f31b353602f2f2a3293809a02d5204eab59c9be2
Stephanie Smith <stephanie.lauren.design@gmail.com>
2019-02-26 08:46:13 -0800

    more config

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,4 +1,5 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ngw = require('@ngtools/webpack');
 
 module.exports = {
     entry: './src/app/main.ts',
@@ -8,17 +9,25 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /(?:\.ngfactory\.js|\.ngstyle\.js|\.ts)$/,
+                loader: '@ngtools/webpack'
+            },
+            {
                 test: /\.html$/,
                 loaders: ['html-loader']
             },
             {
                 test: /\.css$/,
                 loaders: ['raw-loader']
-            }
+            },
         ],
         exprContextCritical: false
     },
     plugins: [
+        new ngw.AngularCompilerPlugin({
+            tsConfigPath: './tsconfig.aot.json',
+            entryModule: './src/app/app.module#AppModule'
+        }),
         new HtmlWebpackPlugin({
             template: 'src/index.html'
         })
